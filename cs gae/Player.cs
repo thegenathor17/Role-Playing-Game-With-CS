8e6619d660675880f9fa4141ab1a65904f16272d
Nahum <toodoo1703@gmail.com>
2024-01-25 21:18:31 -0600

    Indev Advances 1

--- cs gae/Player.cs
+++ cs gae/Player.cs
@@ -3,6 +3,7 @@ namespace cs_gae
 {
     internal class Player
     {
+        #region variables
         //variables
         public string Nombre { get; set; }
         public int HabilidadConElArco { get; set; }
@@ -16,6 +17,8 @@ namespace cs_gae
         public int[,] Inventario = new int[7,7];
         public int VidaMaxima { get; set; }
         //variables
+        #endregion
+        #region constructores
         //constructores
         public Player(string nombre, int defensa, int vida, int vidaMaxima, int magia, int nivel, int experiencia)
         {
@@ -28,18 +31,27 @@ namespace cs_gae
             this.Experiencia = experiencia;
         }
         //constructores
+        #endregion
+        #region Metodos
         //metodos
-        public static void DisplayStats(string nombre, int ataque, int defensa, int vidaMaxima, int magia, int nivel)
+        public void DisplayStats(string nombre, int habilidadConLaEspada, int habilidadConElArco, int defensa, int vidaMaxima, int magia, int nivel, int experiencia)
         {
-            Console.WriteLine($"Estadisticas de {nombre} son: \nAtaque: {ataque}\nDefensa: {defensa}\nVida Maxima {vidaMaxima}\nMagia: {magia}\nNivel: {nivel}");
+            if (habilidadConElArco != 0)
+            {
+                Console.WriteLine($"The statistics of {nombre} are: \nBow Hability: {habilidadConElArco}\nDefense: {defensa}\nMax Healt: {vidaMaxima}\nMagic: {magia}\nLevel: {nivel}\nActual XP: {experiencia}");
+            }
+            else
+            {
+                Console.WriteLine($"The statistics of {nombre} are: \nSword Hability: {habilidadConLaEspada}\nDefense: {defensa}\nMax Healt: {vidaMaxima}\nMagic: {magia}\nLevel: {nivel}\nActual XP: {experiencia}");
+            }
         }
         public void DisplayInventory()
         {
-            for (int i = 0; i > Inventario.Length; i++)
+            for (int i = 0; i < Inventario.Length; i++)
             {
-                for(int j = 0; j < Inventario.Length; j++)
+                for (int j = 0; j < Inventario.Length; j++)
                 {
-                    Console.WriteLine($"{Inventario[i,j]}");
+                    Console.WriteLine($"{Inventario[i, j]}");
                 }
             }
         }
@@ -56,13 +68,27 @@ namespace cs_gae
                 Console.WriteLine($"Now you have {saludDelEnemigo - (-defensaDelEnemigo)} points of healt.");
             }
         }
+        public void AjusteDeVida(int curacion)
+        {
+            Vida += curacion;
+            if (Vida > VidaMaxima)
+            {
+                Vida = Vida - (VidaMaxima - Vida);
+            }
+        }
+        public void Defend()
+        {
+            Console.WriteLine("The next turn you not going to receive damage :)");
+        }
         //metodos
+        #endregion
+        #region destructor
         //destructor
         ~Player()
         {
             Console.WriteLine("Has muerto");
-            estaVivo = false;
         }
         //destructor
+        #endregion
     }
 }
--- cs gae/Program.cs
+++ cs gae/Program.cs
@@ -1,81 +1,68 @@
-﻿using System.Data.Common;
-using System.Runtime.Serialization.Json;
-using System.ComponentModel.Design.Serialization;
-using System.Runtime.InteropServices;
-using System.Reflection.Metadata;
-using System.Net.Http.Headers;
-using System.Collections.Concurrent;
-using System.Runtime.Serialization;
-using System.Reflection.Emit;
-using System.Data;
-using System;
-using System.IO;
-using cs_gae;
+﻿using cs_gae;
 using System.Text.Json;
 namespace text_Based_Game_With_CS
 {
+
     class Program
     {
+        int[] saveProgress = new int[90];
 
+        string saveName = "";
         //game menu
-        static void Main(string[] args)
+        void Main()
         {
-            Console.WriteLine("Welcome!");
-            Console.WriteLine("Do you want to start a new game or do you want to continue with the saved file? 1. Continue 2. Start new game");
-            string choice = Console.ReadLine();
-            if (choice == "1")
+            while (true)
             {
-                Console.WriteLine("Loading...");
-                LoadGame();
-            }
-            else if (choice == "2")
-            {
-                Game();
-            }
-            else
-            {
-                Console.WriteLine("What?");
-            }
-        }
-        static void GameMenu()
-        {
-            Console.WriteLine("Do you want to start a new game or do you want to continue with the saved file? 1. Continue 2. Start new game");
-            string choice = Console.ReadLine();
-            if (choice == "1")
-            {
-                Console.WriteLine("Loading...");
-                LoadGame();
-            }
-            else if (choice == "2")
-            {
-                Game();
-            }
-            else
-            {
-                Console.WriteLine("What?");
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("Do you want to start a new game or do you want to continue with the saved file? 1. Continue 2. Start new game");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.WriteLine("Loading...");
+                    LoadGame();
+                }
+                else if (choice == "2")
+                {
+                    Prologue();
+                }
+                else if (choice == "Admin: Nahum")
+                {
+                    ProbarComandos();
+                }
+                else
+                {
+                    Console.WriteLine("What?");
+                }
             }
         }
         //game menu
         //game
-        static void Game()
+        void Prologue()
         {
-            string choice = "";
-            Console.WriteLine("Deep within this dungeon, a legendary hero has been captured by orcs.");
+            
+            Player jugador = new("juan", 3, 25, 100, 1, 1, 0);
+            Console.WriteLine("Deep within this dungeon, a legendary hero has been captured by orcs.");  //Primeros dialogos
             Console.WriteLine("A group of brave adventurers has arrived to free him and face the orcish threat.");
             Console.WriteLine("The battle in the dungeon is about to begin!");
-            Console.WriteLine("The legendary hero needs a name, what is it?");
-            Player jugador = new(Console.ReadLine(), 3, 25, 0, 1, 0, 0);
+            Console.WriteLine("The legendary hero needs a name, what is it?");                           //Primeros dialogos
+            string choice = Console.ReadLine(); //Toma el nombre del jugador
+            jugador.Nombre = choice; //Cambia el nombre del jugador
+            Comandos(choice, jugador); //llama a comandos (en progreso)
             Console.WriteLine($"{jugador.Nombre}? Good name");
-            Console.WriteLine("You wake up after a very loud noise, it was the door being knocked down by one of your best friends while the fight is heard in the background.");
+            Console.WriteLine("You wake up after a very loud noise, it was the door being knocked down by one of your best friends while the fight is heard in the background.");//Segundo conjunto de dialogos
+            //Inicializa a los compañeros
             Human aldric = new(15, 15, 75, "Aldric");
             Human cedric = new(10, 15, 80, 15, "Cedric");
             Human roderick = new(10, 15, 80, "roderick");
             Console.WriteLine($"{jugador.Nombre} wake up");
+            //Inicializa a los compañeros
+            //Primer QAE
             Console.WriteLine("You wake up still asleep and start walking towards the door unsteadily.");
             while (true)
             {
                 Console.WriteLine($"Quick, {jugador.Nombre} choose a weapon: 1.Sword 2. Bow and Arrow");
                 choice = Console.ReadLine();
+                Comandos(choice, jugador);
                 if (choice == "1")
                 {
                     Console.WriteLine("Nice!");
@@ -95,9 +82,102 @@ namespace text_Based_Game_With_CS
                     Console.WriteLine("What?");
                 }
             }
+            //Primer QAE
+            //Primer Combate (temporal)
             Orc enemigoTutorial = new Orc(10, 3, 30);
+            Console.WriteLine("Checkpoint reached.");
+            SaveGame(jugador, saveProgress);
+            Console.WriteLine("You run towards the orc but he easily rejects you.");
+            Console.WriteLine("");
+            Console.WriteLine("Combat started, here is a little information: ");
+            while (true)
+            {
+                jugador.DisplayStats(jugador.Nombre, jugador.HabilidadConLaEspada, jugador.HabilidadConElArco, jugador.Defensa, jugador.VidaMaxima, jugador.Magia, jugador.Nivel, jugador.Experiencia);
+                
+                Comandos(choice, jugador);
+
+            }
+            //Primer Combate (temporal)
 
         }
+        #region Comandos
+        void ProbarComandos()
+        {
+            Player jugador = new Player("juan", 3, 25, 0, 1, 0, 0);
+            Console.WriteLine("Si no eres yo, quien eres?");
+            Console.WriteLine("Como sea, bienvenido al probador de comandos");
+            Console.WriteLine("Que comando probaremos hoy?");
+            string comando = Console.ReadLine();
+            switch (comando)
+            {
+                case "Menu" or "menu":
+                    Main();
+                    break;
+                case "Save" or "save":
+                    SaveGame(jugador, saveProgress);
+                    break;
+                case "defend" or "Defend":
+                    jugador.Defend();
+                    break;
+                case "attack" or "Attack":
+                    jugador.Attack(1,2,"tuculo",false);
+                    break;
+                default:
+                    Console.WriteLine("Uh?");
+                    break;
+            }
+            while(true)
+            {
+                Console.WriteLine("Que otro comando probaremos hoy?");
+                comando = Console.ReadLine();
+                switch (comando)
+                {
+                    case "Menu" or "menu":
+                        Main();
+                        break;
+                    case "Save" or "save":
+                        SaveGame(jugador, saveProgress);
+                        break;
+                    case "Defend" or "defend":
+                        jugador.Defend();
+                        break;
+                    case "attack" or "Attack":
+                        jugador.Attack(1, 2, "tuculo", false);
+                        break;
+                    default:
+                        Console.WriteLine("Uh?");
+                        break;
+                }
+            }
+            Main();
+        }
+        void Comandos(string comando, Player jugador)
+        {
+            switch (comando)
+            {
+                case "Menu" or "menu":
+                    Main();
+                    break;
+                case "Save" or "save":
+                    SaveGame(jugador, saveProgress);
+                    break;
+            }
+        }
+        static void ComandosEnCombate(string comando, Player jugador, int saludDelEnemigo, int defensaDelEnemigo, string enemigo, bool arco)
+        {
+            switch (comando)
+            {
+                case "attack" or "Attack":
+                    jugador.Attack(saludDelEnemigo, defensaDelEnemigo, enemigo, arco);
+                    break;
+                case "Defend" or "defend":
+                    jugador.Defend();
+                    break;
+                case "" or "":
+                    break;
+            }
+        }
+        #endregion
         //game
         #region //SaveLoadManager
         //saves
@@ -105,28 +185,35 @@ namespace text_Based_Game_With_CS
         {
             try
             {
-                Player loadedplayer = LoadPlayerData("Saves.json");
-                Console.WriteLine("Loading...");
-                Console.WriteLine("Loaded");
+               
             }
             catch (System.IO.FileNotFoundException)
             {
-                Console.WriteLine("Error 001. No save file.");
-                Console.WriteLine("Redirecting to Main menu...");
-                GameMenu();
+                
             }
         }
-        static void SaveGame(Player gameProgress, string filePath)
+        static void SneekLoadGame()
         {
-            string jsonString = JsonSerializer.Serialize(gameProgress);
-            File.WriteAllText(filePath, jsonString);
-            Console.WriteLine("Game progress saved successfully.");
+            try
+            {
+                
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+        static void SaveGame(Player playerStatistics, int[] Gameprogress)
+        {
+            
         }
         static Player LoadPlayerData(string filepath)
         {
-            string jsonString = File.ReadAllText(filepath);
-            Player loadedPlayer = JsonSerializer.Deserialize<Player>(jsonString);
-            return loadedPlayer;
+            
         }
         //saves
         #endregion
